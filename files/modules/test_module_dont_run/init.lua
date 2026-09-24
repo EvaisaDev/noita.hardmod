897d7f23e20be9599b9c9976a91e2c5182119d3f
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-09-25 00:28:31 +0100

    added hook system + NXML

---
+++ files/modules/test_module_dont_run/init.lua
@@ -0,0 +1,49 @@
+--This is just a module to test the init hook system.
+
+
+local hooks = {
+	--Game/World Initialising
+	mod_pre_init = {},
+	mod_init = {},
+	mod_post_init = {},
+	magic_numbers_and_seed_initialised = {}, --OnMagicNumbersAndWorldSeedInitialised
+	edit_material = {}, --iterates over all materials OnMagicNumbersAndWorldSeedInitialised
+	edit_reaction = {}, --iterates over all reactions OnMagicNumbersAndWorldSeedInitialised
+	biome_config = {},
+	world_init = {},
+
+	--Runtime hooks
+	pre_update = {}, --beginning of every frame
+	new_eid = {}, --new entity
+	post_update = {}, --end of every frame
+
+	--Player hooks
+	player_spawned = {}, --When the player spawns in after world is initialised
+	player_changed = {}, --Runs whenever polymorphing/unpolymorphing
+	player_destroyed = {}, --OnPlayerDied
+
+	--Pause hooks
+	mod_settings_changed = {},
+	pause_pre_update = {},
+	pause_changed = {},
+	count_secrets = {},
+}
+
+for hook_name,_ in pairs(hooks) do
+	hooks[hook_name] = {function(...)
+		if ... ~= nil then
+			local vals = {...}
+			for i,v in ipairs(vals) do
+				vals[i] = tostring(v)
+			end
+			print(("[%s]: (%s)"):format(hook_name, table.concat(vals, ", ")))
+		else
+			print(("[%s]"):format(hook_name))
+		end
+		return ...
+	end}
+end
+
+print("raw init")
+
+return hooks
--- init.lua
+++ init.lua
@@ -1,2 +1,245 @@
 -- TODO: make a mod :)
 -- Eba was here!!!
+-- UserK is also around!
+
+
+dofile_once("mods/noita.hardmod/lib/utilities.lua")
+local nxml = dofile_once("mods/noita.hardmod/lib/nxml/nxml.lua") ---@type nxml
+
+local modules = {
+	--"mods/noita.hardmod/files/modules/test_module_dont_run/init.lua",
+}
+
+
+
+local hooks = {
+	--Game/World Initialising
+	mod_pre_init = {}, --OnModPreInit
+	mod_init = {}, --OnModInit
+	mod_post_init = {}, --OnModPostInit
+	magic_numbers_and_seed_initialised = {}, --OnMagicNumbersAndWorldSeedInitialized
+	edit_material = {}, --iterates over all materials after magic_numbers_and_seed_initialised
+	edit_reaction = {}, --(DISABLED UNTIL NEEDED) iterates over all reactions after magic_numbers_and_seed_initialised
+	biome_config = {}, --OnBiomeConfigLoaded
+	world_init = {}, --OnWorldInitialised
+
+	--Runtime hooks
+	pre_update = {}, --OnWorldPreUpdate (start of every frame)
+	new_eid = {}, --on new entity created after pre_update
+	post_update = {}, --OnWorldPreUpdate (end of every frame)
+
+	--Player hooks
+	player_spawned = {}, --OnPlayerSpawned
+	player_changed = {}, --on player polymorphed/unpolymorphed
+	player_destroyed = {}, --OnPlayerDied
+
+	--Pause hooks
+	mod_settings_changed = {}, --OnModSettingsChanged
+	pause_pre_update = {}, --OnPausePreUpdate
+	pause_changed = {}, --OnPausedChanged
+	count_secrets = {}, --OnCountSecrets
+}
+
+GLOBAL_DATA = {
+	player = nil, --player EID.
+	player_poly_identity = nil, --contains some data on the player if they are polymorphed, nil if not polymorphed.
+	player_does_not_exist = true, --this is set to false on `player_spawned` and set to true on `player_destroyed`.
+
+	frame = 0,
+}
+local gd = GLOBAL_DATA
+
+for _,module in ipairs(modules) do
+	for hook_name,module_hooks in pairs(dofile(module) or {}) do
+		for _,hook in ipairs(module_hooks) do
+			hooks[hook_name][#hooks[hook_name]+1] = hook
+		end
+	end
+end
+
+
+function OnModPreInit()
+	for _,func in ipairs(hooks.mod_pre_init) do
+		func()
+	end
+end
+function OnModInit()
+	for _,func in ipairs(hooks.mod_init) do
+		func()
+	end
+end
+function OnModPostInit()
+	for _,func in ipairs(hooks.mod_post_init) do
+		func()
+	end
+end
+
+function OnMagicNumbersAndWorldSeedInitialized() -- this is the last point where the Mod* API is available. after this materials.xml will be loaded.
+	for _,func in ipairs(hooks.magic_numbers_and_seed_initialised) do
+		func()
+	end
+
+	-- this code adds tags to preexisting materials, its good for compatibility
+	local files = ModMaterialFilesGet()
+	for _, file in ipairs(files) do --add modded materials
+		for xml in nxml.edit_file(file) do
+			for elem in xml:each_child() do
+				local elem_name = elem.name
+				if elem_name == "CellData" or elem_name == "CellDataChild" then
+					for _,func in ipairs(hooks.edit_material) do
+						func(elem)
+					end
+				else
+					--for _,func in ipairs(hooks.edit_reaction) do
+					--	func(elem)
+					--end disabled until someone actually needs it for something
+				end
+			end
+		end
+	end
+end
+
+function OnBiomeConfigLoaded()
+	for _,func in ipairs(hooks.biome_config) do
+		func()
+	end
+end
+
+function OnWorldInitialized()
+	for _,func in ipairs(hooks.world_init) do
+		func()
+	end
+end
+
+
+function OnPlayerSpawned(p)
+	gd.player = p
+	gd.player_does_not_exist = false
+	local is_run_start = GameHasFlagRun("noita.hardmod.on_player_spawned_flag")
+	for _,func in ipairs(hooks.player_spawned) do
+		func(is_run_start)
+	end
+
+	GameAddFlagRun("noita.hardmod.on_player_spawned_flag")
+end
+
+function OnPlayerDied(p)
+	gd.player_does_not_exist = true
+	gd.player = nil
+	gd.player_poly_identity = nil
+	for _,func in ipairs(hooks.player_destroyed) do
+		func()
+	end
+end
+
+local prev_max_eid = -1
+local max_eid = -1
+local check_entities = function()
+	local old_player = gd.player
+	local borked_polymorph_entity
+	if not (gd.player_does_not_exist or EntityGetIsAlive(gd.player)) then
+		max_eid = prev_max_eid --rollback in case EntityGetIsAlive was outdated by 1 frame (happens).
+		gd.player = nil
+	end
+
+	local new_max = EntitiesGetMaxID()
+	---@type entity_id
+	for i = max_eid + 1, new_max do
+		if not (gd.player or gd.player_does_not_exist) then
+			if EntityHasTag(i, "player_unit") then
+				gd.player = i
+				gd.player_poly_identity = nil
+			else
+				if EntityHasTag(i, "polymorphed_player") then
+					gd.player = i
+					gd.player_poly_identity = {
+						path = EntityGetFilename(i),
+						name = EntityGetName(i)
+					}
+				else
+					for _,comp in ipairs(EntityGetComponent(i, "GameStatsComponent") or {}) do
+						if ComponentGetTypeName(comp) ~= "GameStatsComponent" then --Check if GSC is bugged (store as backup if yes)
+							borked_polymorph_entity = borked_polymorph_entity or i
+							goto continue
+						elseif ComponentGetValue2(comp, "is_player") then
+							gd.player = i
+							gd.player_poly_identity = {
+								path = EntityGetFilename(i),
+								name = EntityGetName(i)
+							}
+							goto continue
+						end
+					end
+				end
+			end
+			::continue::
+		end
+		for _,func in ipairs(hooks.new_eid) do
+			local varcomp_tree = {}
+			for _,varcomp in ipairs(EntityGetComponent(i, "VariableStorageComponent") or {}) do
+				local name = ComponentGetValue2(varcomp, "name")
+				varcomp_tree[name] = varcomp_tree[name] or {}
+				varcomp_tree[name][#varcomp_tree[name]+1] = varcomp
+			end
+			func(i, varcomp_tree)
+		end
+	end
+	prev_max_eid = max_eid --we store prev max eid in case a rollback is necessary due to frame delay nonsense with EntityGetIsAlive.
+	max_eid = new_max
+
+	if old_player ~= gd.player then
+		if gd.player == nil and borked_polymorph_entity then
+			gd.player = borked_polymorph_entity
+			gd.player_poly_identity = {
+				path = EntityGetFilename(gd.player),
+				name = EntityGetName(gd.player)
+			}
+		end
+		if gd.player ~= nil then
+			for _,func in ipairs(hooks.player_changed) do
+				func()
+			end
+		end
+	end
+end
+
+function OnWorldPreUpdate()
+	gd.frame = GameGetFrameNum()
+	check_entities()
+
+	for _,func in ipairs(hooks.pre_update) do
+		func(gd.frame)
+	end
+end
+
+function OnWorldPostUpdate()
+	for _,func in ipairs(hooks.post_update) do
+		func(gd.frame)
+	end
+end
+
+
+function OnModSettingsChanged()
+	for _,func in ipairs(hooks.mod_settings_changed) do
+		func()
+	end
+end
+
+function OnPausePreUpdate()
+	for _,func in ipairs(hooks.pause_pre_update) do
+		func()
+	end
+end
+
+function OnPausedChanged(is_paused, is_inventory_pause)
+	for _,func in ipairs(hooks.pause_changed) do
+		func(is_paused, is_inventory_pause)
+	end
+end
+
+function OnCountSecrets(total, found)
+	for _,func in ipairs(hooks.count_secrets) do
+		total, found = func(total, found)
+	end
+	return total, found
+end
